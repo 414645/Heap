d4de363fbfb57e98d79734529ac484ef850a4bca
414645 <414645@bsd48.org>
2023-02-22 13:35:18 -0800

    User Input

--- main.cpp
+++ main.cpp
@@ -11,4 +11,41 @@ using namespace std;
 
 int main() {
   cout << "hello world" << endl;
+  bool quit = false;
+  char input[80];
+
+  //our heap (101 since ignoring 0th spot)
+  int heap[101];
+  while (quit == false) {
+    cin.getline(input, 80);
+    if (strcmp (input, "HELP") == 0) {
+      cout << "commands are ADD, DELETE, CLEAR, and QUIT" << endl;
+    }
+    if (strcmp (input, "ADD") == 0) {
+      cout << "would you like add from a file? (y/n)" << endl;
+      cin.getline(input, 80);
+      if (strcmp(input, "Y") == 0 || strcmp(input, "Yes") == 0 ||
+	  strcmp(input, "y") == 0 || strcmp(input, "yes") == 0 ||
+	  strcmp(input, "YES") == 0) {
+	cout << "file" << endl;
+	//add from a file
+      }
+      else {
+	cout << "console" << endl;
+	//add from a file
+      }
+    }
+    if (strcmp (input, "DELETE") == 0) {
+      cout << "del" << endl;
+      //delete largest element and cout it
+    }
+        if (strcmp (input, "CLEAR") == 0) {
+      cout << "clear" << endl;
+      //delete largest element and cout it until heap is empty
+    }
+    
+    if (strcmp (input, "QUIT") == 0) {
+      quit = true;
+    }
+  }
 }
